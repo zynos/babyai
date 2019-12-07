bd7db3b4b640dbdbaf1effef22d826ea77c76f4c
pedro pann <pedro.pann@web.de>
2019-12-07 21:03:19 +0100

    added package import for install

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.2',
     license='BSD 3-clause',
     keywords='memory, environment, agent, rl, openaigym, openai-gym, gym',
-    packages=['babyai', 'babyai.levels', 'babyai.utils'],
+    packages=['babyai', 'babyai.levels', 'babyai.utils','myScripts'],
     install_requires=[
         'gym>=0.9.6',
         'numpy==1.15.4', # Temporary: fix numpy version because of bug introduced in 1.16
