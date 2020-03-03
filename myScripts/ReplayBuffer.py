41875762c2aff1ed5571685cda785a1b4d28f97c
pedro pann <pedro.pann@web.de>
2020-03-03 23:18:39 +0100

    mem leak

--- myScripts/ReplayBuffer.py
+++ myScripts/ReplayBuffer.py
@@ -1,6 +1,9 @@
 import torch
 from scipy.stats import rankdata
 import numpy as np
+from torch.distributions import Categorical
+
+
 class ProcessData():
     def __init__(self):
         # input proc_id * feature
@@ -36,15 +39,21 @@ class ProcessData():
         torch.cuda.empty_cache()
         del self.instructions
         torch.cuda.empty_cache()
+        del self.loss
+        torch.cuda.empty_cache()
+        del self.returnn
+        torch.cuda.empty_cache()
 
 
 class ReplayBuffer:
     def __init__(self, nr_procs):
+        self.sample_amount = 8
         self.nr_procs = nr_procs
-        self.max_size = 128
         self.added_episodes = 0
         self.proc_data_buffer = [ProcessData() for _ in range(self.nr_procs)]
+        self.max_size = 128
         self.replay_buffer = [None] * self.max_size
+        self.big_counter=0
 
     def buffer_full(self):
         return self.added_episodes == self.max_size
@@ -62,11 +71,11 @@ class ReplayBuffer:
     def get_combined_ranks(self,losses,returns):
         # we need the deviation of the mean return per episode
         mean_return = np.mean(returns)
-        ret_deviations = [mean_return - ret for ret in returns]
+        ret_deviations = [np.abs(mean_return - ret) for ret in returns]
 
         ranked_losses = rankdata(losses)
         ranked_ret_deviations = rankdata(ret_deviations)
-        combined_ranks = ranked_losses = ranked_ret_deviations + ranked_losses
+        combined_ranks = ranked_ret_deviations + ranked_losses
         return combined_ranks
 
     def get_losses_and_returns(self):
@@ -93,10 +102,25 @@ class ReplayBuffer:
         lowest_rank, low_index = self.get_lowest_ranking_and_idx(combined_ranks)
         # if lowest ranked episode is lower than the new episode add it to buffer
         if lowest_rank < new_episode_rank:
-            print("replace with",lowest_rank,new_episode_rank)
-            self.replay_buffer[lowest_rank] = episode
-        else:
+            dummy_episode=ProcessData()
+            dummy_episode.loss=episode.loss
+            dummy_episode.returnn = episode.returnn
+            dummy_episode.embeddings=episode.embeddings
+            episode.self_destroy()
             del episode
+            # print("replace with",lowest_rank,new_episode_rank)
+            # self.replay_buffer[low_index].self_destroy()
+            self.replay_buffer[low_index] = dummy_episode
+            self.added_new_episode=True
+            self.big_counter+=1
+            print("big counter",self.big_counter)
+            if self.big_counter>=200:
+                self.added_episodes=0
+                self.replay_buffer = None
+                self.replay_buffer = [None] * self.max_size
+        else:
+            self.added_new_episode = True
+        #     del episode
 
     def add_data_to_process_buffer(self, data_list):
         # data is embeddings,actions,rewards,dones,instructions,images
@@ -108,8 +132,9 @@ class ReplayBuffer:
             if self.proc_data_buffer[proc_id].dones[-1] == True:
                 procs_to_init.append(proc_id)
                 complete_episodes.append(self.proc_data_buffer[proc_id])
+        # print("complete episodes",len(complete_episodes))
         self.init_process_data(procs_to_init)
-        del data_list
+        # del data_list
         return complete_episodes
 
     def init_process_data(self, procs_to_init):
@@ -122,5 +147,17 @@ class ReplayBuffer:
     def add_timestep_data(self, embeddings, actions, rewards, dones, instructions, images):
         result = self.detach_and_clone(embeddings, actions, rewards, dones, instructions, images)
         complete_episodes = self.add_data_to_process_buffer(result)
-        del result
+        # del result
         return complete_episodes
+
+    def sample_episodes(self):
+        losses,retruns=self.get_losses_and_returns()
+        combined_ranks= self.get_combined_ranks(losses,retruns)
+        probs=torch.nn.functional.softmax(torch.tensor(combined_ranks))
+        m = Categorical(probs)
+        episodes=[]
+        for _ in range(self.sample_amount):
+            episode_id = m.sample()
+            episodes.append(self.replay_buffer[episode_id])
+        return episodes
+
--- myScripts/Rudder.py
+++ myScripts/Rudder.py
@@ -10,17 +10,26 @@ class Rudder:
         self.optimizer = torch.optim.Adam(self.net.parameters(), lr=1e-4, weight_decay=1e-4)
         self.device = device
         self.first_training_done = False
+        self.mu=20
+        self.quality_threshold=0.8
+
+
+    def calc_quality(self,diff):
+        # diff is g - gT_hat -->  see rudder paper A267
+        quality=1-(np.abs(diff.item())/self.mu)*1/(1-self.quality_threshold)
+        return quality
+
 
     def lossfunction(self, predictions, returns):
+        diff=predictions[:, -1] - returns
         # Main task: predicting return at last timestep
-        main_loss = torch.mean(predictions[:, -1] - returns) ** 2
+        quality=self.calc_quality(diff)
+        main_loss = torch.mean(diff) ** 2
         # Auxiliary task: predicting final return at every timestep ([..., None] is for correct broadcasting)
-        if returns.item() > 0:
-            print('d')
         aux_loss = torch.mean(predictions[:, :] - returns[..., None]) ** 2
         # Combine losses
         loss = main_loss + aux_loss * 0.5
-        return loss
+        return loss,quality
 
     def predict_every_timestep(self, episode: ProcessData):
         hidden = None
@@ -36,29 +45,29 @@ class Rudder:
     def feed_network(self, episode: ProcessData):
         predictions = self.predict_every_timestep(episode)
         returns = torch.sum(torch.tensor(episode.rewards, device=self.device), dim=-1)
-        loss = self.lossfunction(predictions, returns)
-        return loss, returns
+        loss,quality = self.lossfunction(predictions, returns)
+        return loss, returns,quality
 
     def train_one_episode(self, episode: ProcessData):
-        loss, returns = self.feed_network(episode)
+        loss, returns,quality = self.feed_network(episode)
 
         loss.backward()
         self.optimizer.step()
         self.optimizer.zero_grad()
 
         loss = loss.detach().item()
-        returnn = returns.item()
+        returnn = returns.detach().item()
 
-        return loss, returnn
+        return loss, returnn,quality
 
 
 
     def add_to_buffer_or_discard(self, new_episode):
         # get loss and return of this episode
         with torch.no_grad():
-            loss, returnn = self.feed_network(new_episode)
-            new_episode.loss = loss
-            new_episode.returnn = returnn
+            loss, returnn,quality = self.feed_network(new_episode)
+            new_episode.loss = loss.detach().item()
+            new_episode.returnn = returnn.detach().item()
         self.replay_buffer.try_to_replace_old_episode(new_episode)
 
 
@@ -66,33 +75,45 @@ class Rudder:
 
 
 
-
+    def fill_empy_buffer(self, complete_episode):
+        self.replay_buffer.replay_buffer[self.replay_buffer.added_episodes] = complete_episode
+        self.train_and_set_metrics(complete_episode)
+        self.replay_buffer.added_episodes += 1
+        print("added ", self.replay_buffer.added_episodes)
 
 
     def consider_adding_complete_episodes_to_buffer(self, complete_episodes):
         for ce in complete_episodes:
             if not self.replay_buffer.buffer_full():
-                self.replay_buffer.replay_buffer[self.replay_buffer.added_episodes] = ce
-                self.replay_buffer.added_episodes += 1
-                print("added ",self.replay_buffer.added_episodes)
+                self.fill_empy_buffer(ce)
             else:
+                if self.replay_buffer.buffer_full():
+                    if not self.first_training_done:
+                        self.train_full_buffer()
+                        self.first_training_done = True
                 self.add_to_buffer_or_discard(ce)
 
-        del complete_episodes
+        # del complete_episodes
+
+    def train_and_set_metrics(self, episode):
+        loss, returnn, quality = self.train_one_episode(episode)
+        episode.loss = loss
+        episode.returnn = returnn
+        print("loss", loss)
+        return quality > 0
 
     def train_full_buffer(self):
-        episodes = self.replay_buffer.replay_buffer
-        for episode in episodes:
-            print("train one ep")
-            loss, returnn = self.train_one_episode(episode)
-            episode.loss = loss
-            episode.returnn = returnn
-            print("loss",loss)
+        qualities=set()
+        for epoch in range(5):
+            episodes = self.replay_buffer.sample_episodes()
+            for episode in episodes:
+                quality=self.train_and_set_metrics(episode)
+                qualities.add(quality)
+
+        if False in qualities:
+            self.train_full_buffer()
 
     def add_timestep_data(self, *args):
         complete_episodes = self.replay_buffer.add_timestep_data(*args)
         self.consider_adding_complete_episodes_to_buffer(complete_episodes)
-        if self.replay_buffer.buffer_full():
-            if not self.first_training_done:
-                self.train_full_buffer()
-                self.first_training_done=True
+
