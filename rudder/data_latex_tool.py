07de9edc3280b55aba361c94fd6160f23174ae56
Max Grosser <maximilian_grosser@apple.com>
2020-10-31 12:06:20 +0100

    updated latx data

--- rudder/data_latex_tool.py
+++ rudder/data_latex_tool.py
@@ -3,44 +3,62 @@ from os import listdir
 import pandas as pd
 from os.path import isfile, join
 
+
 # take name and iterate over seed
 
 def create_summary(dfs):
-    out=[]
+    out = []
     for df in dfs:
-        before_45M = df[df["frames"]<=45000000]
+        before_45M = df[df["frames"] <= 45000000]
         ret_mean = before_45M['return_mean'].mean()
         ret_max = before_45M['return_mean'].max()
 
-        around_03_ret = df[df["return_mean"]>=0.3]
-        frames_min = around_03_ret['frames'].min()/1000000
-        out.append([ret_mean,ret_max,frames_min])
+        around_03_ret = df[df["return_mean"] >= 0.3]
+        frames_min = around_03_ret['frames'].min() / 1000000
+        out.append([ret_mean, ret_max, frames_min])
 
-    out = pd.DataFrame(out, columns=['ret_mean','ret_max','frame_min'])
-    return out
-
-
+    out = pd.DataFrame(out, columns=['ret_mean', 'ret_max', 'frame_min'])
+    return out.describe().transpose()[['mean', 'std']].round(2)
 
 
+def df_to_plus_minus(df):
+    # mystr = " " + u'\u00b1' + " "
+    mystr = " mypm "
+
+    return df[df.columns].apply(lambda x: mystr.join(x.dropna().astype(str)), axis=1)
 
 
-directory = "../scripts/logs/masterThesis/baselineVSStandard/"
+def create_df_slice(data):
+    sum_df = create_summary(data)
+    return df_to_plus_minus(sum_df)
 
-dirs = [f for f in listdir(directory)]
 
-baseline_name = "norud"
+directory = "../scripts/logs/masterThesis/forMac/"
+baseline_name = "Reccurrence"
+
+dirs = [f for f in listdir(directory) if f != '.DS_Store']
+
 baseline_data = []
 to_compared = []
 for d in dirs:
     data = pd.read_csv(directory + d + "/log.csv")[['frames', 'return_mean']]
-    if baseline_name in d.lower():
+    if baseline_name.lower() in d.lower():
         baseline_data.append(data)
     else:
         to_compared.append(data)
 
 assert len(baseline_data) == len(to_compared) == 3
-base_df = create_summary(baseline_data).add_suffix("_base")
-compare_df = create_summary(to_compared).add_suffix("_rudder")
-# final_df = pd.DataFrame.merge(base_df,compare_df,on=['ret_mean', 'ret_max','frame_min'],suffixes=('_base', '_rudder'))
-final_df = pd.concat([base_df, compare_df.reindex(base_df.index)], axis=1).describe().transpose()
+
+base_df = create_df_slice(baseline_data).rename('baseline')
+rud_df = create_df_slice(to_compared).rename('endpool res baseline')
+
+final_df = pd.concat([base_df, rud_df], axis=1)
+latex_str = final_df.to_latex()
+latex_str = latex_str.replace('mypm', '$ \pm $')
+print(latex_str)
 print('d')
+
+#               baseline | run x
+# mean ret      3
+# max ret       3
+# frames min    233
--- rudder/rudder_main.py
+++ rudder/rudder_main.py
@@ -34,10 +34,10 @@ class Rudder:
         self.max_grad_norm = 0.5
         self.mu = 1
         self.quality_threshold = 0.85
-        use_widi = False
+        use_widi = True
         dummy_args = NonParsedDummyArgs(instr_dim, memory_dim, image_dim,base_rl_algo.acmodel.lang_model, model_name,lr)
-        use_residual = True
-        use_endpool = True
+        use_residual = False
+        use_endpool = False
         self.il_learn = RudderImitation(None, True, True,False,use_widi,use_endpool,use_residual, dummy_args)
         # these 2 must be updated when replaybuffer full and then after every new insert
         self.il_learn.mean = 0
@@ -104,9 +104,9 @@ class Rudder:
         loss, seq_return, (aux, main, predictions, quality) = self.get_loss_for_batch(my_obs, my_masks, my_rewards,
                                                                                       my_actions, my_values, my_dones,
                                                                                       is_training=False)
-        if update % 200 == 0:
-            self.visualize_current_reward_redistribution(loss, my_obs, my_actions, my_rewards, aux, main, predictions,
-                                                         update, my_dones, model_name)
+        # if update % 200 == 0:
+        #     self.visualize_current_reward_redistribution(loss, my_obs, my_actions, my_rewards, aux, main, predictions,
+        #                                                  update, my_dones, model_name)
         for i in range(my_actions.shape[0]):
             masks_, rewards_, values_, actions_, obs_, seq_return_, final_loss, dones_ = my_masks[i], my_rewards[i], \
                                                                                          my_values[i], my_actions[i], \
